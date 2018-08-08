8b75f35826e8c53fb3bee780938f2e503f32ff32
侯光强 <houguangqiang@shsnc.com>
2018-08-08 19:10:57 +0800

    Fix bug

--- src/com/seventh7/mybatis/provider/MapperLineMarkerProvider.java
+++ src/com/seventh7/mybatis/provider/MapperLineMarkerProvider.java
@@ -8,7 +8,6 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.util.CommonProcessors;
 import com.intellij.util.xml.DomElement;
-import com.seventh7.mybatis.dom.model.IdDomElement;
 import com.seventh7.mybatis.service.JavaService;
 import com.seventh7.mybatis.util.Icons;
 import com.seventh7.mybatis.util.JavaUtils;
@@ -25,9 +24,9 @@ public class MapperLineMarkerProvider extends RelatedItemLineMarkerProvider {
     @Override
     protected void collectNavigationMarkers(@NotNull PsiElement element, Collection<? super RelatedItemLineMarkerInfo> result) {
         if (element instanceof PsiNameIdentifierOwner && JavaUtils.isElementWithinInterface(element)) {
-            CommonProcessors.CollectProcessor<IdDomElement> processor = new CommonProcessors.CollectProcessor<>();
+            CommonProcessors.CollectProcessor<DomElement> processor = new CommonProcessors.CollectProcessor<>();
             JavaService.getInstance(element.getProject()).processMapperInterfaceElements(element, processor);
-            Collection<IdDomElement> results = processor.getResults();
+            Collection<DomElement> results = processor.getResults();
             if (!results.isEmpty()) {
                 PsiElement nameIdentifier = ((PsiNameIdentifierOwner) element).getNameIdentifier();
                 if (nameIdentifier != null) {
@@ -37,7 +36,7 @@ public class MapperLineMarkerProvider extends RelatedItemLineMarkerProvider {
         }
     }
 
-    private NavigationGutterIconBuilder<PsiElement> setupBuilder(Collection<IdDomElement> results) {
+    private NavigationGutterIconBuilder<PsiElement> setupBuilder(Collection<DomElement> results) {
 
         return NavigationGutterIconBuilder.create(Icons.MAPPER_LINE_MARKER_ICON)
                 .setAlignment(GutterIconRenderer.Alignment.CENTER)
